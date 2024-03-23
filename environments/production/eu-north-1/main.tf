8fee6fc2415a88dec9b66f08bbf3b7bb866757e1
SrikanthBhandary <srikanth.bhandary@gmail.com>
2024-03-23 13:30:20 +0100

    Update main.tf

--- environments/production/eu-north-1/main.tf
+++ environments/production/eu-north-1/main.tf
@@ -5,10 +5,10 @@ provider "aws" {
 module "eks_vpc" {
   source                     = "github.com/srikanthbhandary-teach/terraform-eks-vpc?ref=v1.0.1"
   cluster_name               = "docplanner-demo-production"
-  vpc_cidr                   = var.vpc_cidr
-  availability_zones         = var.availability_zones
-  subnet_private_cidr_blocks = var.subnet_private_cidr_blocks
-  subnet_public_cidr_blocks  = var.subnet_public_cidr_blocks
+  vpc_cidr                   = "10.1.0.0/16"
+  availability_zones         = ["eu-north-1a", "eu-north-1b", "eu-north-1c"]
+  subnet_private_cidr_blocks = ["10.1.1.0/24", "10.1.2.0/24", "10.1.3.0/24"]
+  subnet_public_cidr_blocks  = ["10.1.4.0/24", "10.1.5.0/24", "10.1.6.0/24"]
 }
 
 module "eks_cluster" {            

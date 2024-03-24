9e1c739090b05bb59f5769081431e60d5e8da034
SrikanthBhandary <srikanth.bhandary@gmail.com>
2024-03-24 22:27:31 +0100

    Update main.tf

--- environments/testing/eu-north-1/main.tf
+++ environments/testing/eu-north-1/main.tf
@@ -28,16 +28,3 @@ module "eks_cluster" {
   tags = var.tags 
 }
 
-module "lb_role" {
-  source    = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
-  role_name = "${var.cluster_name}-${var.env_name}_eks_lb"
-  attach_load_balancer_controller_policy = true
-
-  oidc_providers = {
-    main = {
-      provider_arn               = module.eks_cluster.oidc_provider_arn
-      namespace_service_accounts = ["kube-system:aws-load-balancer-controller"]
-    }
-  }
-}
-
